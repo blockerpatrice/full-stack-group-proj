6e5aabd62e248474e269dcd9dd7ca8c8ce77ce9b
Benahin Kur <benahink@gmail.com>
2019-06-13 11:47:45 -0600

    commit

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const PORT = 2901;
+const mongoose = require('mongoose');
 
-app.listen()
+
+app.use(express.json());
+
+app.use("/", require("./chatRoute"));
+
+
+mongoose.connect('mongodb://localhost:27017/chatting-app',{useNewUrlParser: true})
+.then(()=> console.log("Connected to MongoDB"))
+.catch(err => console.error(err));
+
+app.listen(PORT, () => {
+   console.log(`App is listening on port ${PORT}`);
+});
