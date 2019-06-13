8057ab96b857a0bc6810494319972118021bb7cf
Benahin Kur <benahink@gmail.com>
2019-06-13 13:10:34 -0600

    commit

--- chatRouter.js
+++ chatRouter.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const chatRouter = express.Router();
+const Chat = require("./models/destination.js");
 
 chatRouter.route("/") 
-
 .post((request, response)=> {
     const newChat = new Chat(request.body);
     newChat.save((err,new_destination) =>{
         if(err) return response.status(500).send(err)
         return response.send(new_destination)
     })
+})
+.get((request, response) => {
+    Chat.find((err,destination) =>{
+        if(err){
+            return response.status(500).send(err)
+        }return response.status(200).send(destination)
+    })
 
 })
+
+module.exports = chatRouter
 
 
