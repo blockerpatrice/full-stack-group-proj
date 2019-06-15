1e36c1c6f85d53222524c148b59ea3ef02f5e995
Benahin Kur <benahink@gmail.com>
2019-06-15 05:16:31 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import MessageList from './MessageList/MessageList.js';
+import MessageList from './messageList/MessageList.js';
 
 class App extends Component {
     constructor() {
